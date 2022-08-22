581a1b8559057abb1d72fea494f8b7c39ee53617
tinepapa <tinembaye@hotmail.com>
2022-08-22 11:44:34 +0000

    using env variables with Travis CI

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+const favoriteFood = process.env.DOCKER_USERNAME;
 async function main() {
   while(true) {
-    console.log('Containers rule!');
+    console.log(`My DockerHub username is ${favoriteFood}`);
     await sleep(5000);
   }
 }
